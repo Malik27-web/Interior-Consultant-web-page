7686361e2373246d100830dfbf879fc4f506985f
Malik27-web <malikbelghith@gmail.com>
2023-06-26 18:14:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var logo=document.getElementById("logo");
+var menulogo=document.getElementById("menulogo");
+var items =document.getElementById("items");
+var croix =document.getElementById("croix");
+var paragraph =document.getElementById("paragraph")
+
+//  if (mediaQuery.matches) {
+
+function myfunction(){
+
+logo.style.display='none';
+items.style.display='flex';
+menulogo.style.display='none';
+croix.style.display='flex';
+paragraph.style.display='none';
+}
+function myfunction2(){
+
+logo.style.display='flex'
+items.style.display='none';
+menulogo.style.display='flex';
+croix.style.display='none';
+paragraph.style.display='flex';
+}
+//  }
+
